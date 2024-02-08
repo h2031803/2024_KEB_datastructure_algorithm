f49ccfdcab271d7b14acbb3436cf1734d415b4b5
h2031803 <h2031803@gmail.com>
2024-02-08 10:27:12 +0900

    v0.2 combination

--- day20.py
+++ day20.py
@@ -1,13 +1,16 @@
 def factorial(number) -> int :
-    pass
+    result = 1
+    for i in range(1, number+1):
+        result = result * i
+    return result
 
 def nCr(n, r) -> int:
     "조합 함수"
     numerator = factorial(n)
-    denominator = factorial(n-r) * factorial(n)
-    return numerator / denominator
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
     print(f"{n}C{r} = {nCr(n,r)}")
