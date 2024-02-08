79dd5446c80278e750238144537f6e25aba19639
h2031803 <h2031803@gmail.com>
2024-02-08 11:03:22 +0900

    v0.6

--- day20.py
+++ day20.py
@@ -1,9 +1,11 @@
 import mymath
+import time
 
 if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
     print(f"{n}C{r} = {mymath.nCr(n,r)}")
+    end = time.time()
 
     # f = int(input())
     # print(mymath.factorial(f))
--- mymath.py
+++ mymath.py
@@ -1,28 +1,32 @@
-# def factorial(number) -> int :
-#     '''
-#     factorial by repetition
-#     :param number: number (int)
-#     :return: factorial result (int)
-#     '''
-#     result = 1
-#     for i in range(1, number+1):
-#         result = result * i
-#     return result
-
-def factorial(number) -> int:
+import time
+def factorial(number) -> int :
     '''
-    factorial by recursion
+    factorial by repetition
     :param number: number (int)
-    :return: factorial result
+    :return: factorial result (int)
     '''
-    if number == 1:
-        return 1
-    else:
-        return number*factorial(number-1)
+    result = 1
+    for i in range(1, number+1):
+        result = result * i
+    return result
+
+# def factorial(number) -> int:
+#     '''
+#     factorial by recursion
+#     :param number: number (int)
+#     :return: factorial result
+#     '''
+#     if number == 1:
+#         return 1
+#     else:
+#         return number*factorial(number-1)
 
 
 def nCr(n, r) -> int:
     "조합 함수"
+    start = time.time()
     numerator = factorial(n)
     denominator = factorial(n-r) * factorial(r)
+    end = time.time()
+    print(f"소요시간 : {end - start}")
     return int(numerator / denominator)
