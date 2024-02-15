1cdd2db4a7d750007c1b7a185eb8601898920f4a
h2031803 <h2031803@gmail.com>
2024-02-15 10:51:39 +0900

    v1.2

--- day20.py
+++ day20.py
@@ -1,22 +1,19 @@
-class Node():
-    def __init__(self):
-        self.data = None
-        self.link = None
+# after 라는 새로운 변수를 생성하고 해당 값에 복제하는 선택정렬 방식
 
+def findMinIdx(ary):
+    minIdx = 0
+    for i in range(1,len(ary)):
+        if(ary[minIdx]>ary[i]):
+            minIdx = i
+    return minIdx
 
-def printNodes(start):
-    current = start
-    if current = None:
-        return
-    print(current.data, end=" ")
-    while(current.link is not None):
-        current = current.link
-        print(current.data, end = " ")
-    print()
+before = [188, 162, 168, 120, 50, 150, 177, 105]
+after = []
 
-def makeSimpleLinkedList(namePhone):
+print('정렬 전 ->',before)
+for _ in range(len(before)):
+    minIdx = findMinIdx(before)     # before  = [ 한개 원소 ]
+    after.append(before[minIdx])    # len(ary) = 1
+    del(before[minIdx])
+print('정렬 후 -->', after)
 
-
-
-head, current, pre = None, None, None
-dataArray = [["지민", "010-1111-1111"], ["정국", "010-2222-2222"], ["뷔", "010-3333-3333"], ["슈가", "010-4444-4444"], ["진", "010-5555-5555"]]
